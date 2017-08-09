71e41b88b978f1719ce5f6069a9a2bca9d34790d
Unknown <salmaanvally72@gmail.com>
2017-08-09 11:59:01 +0200

    method updated to calculate random numbers

--- MIN/MIN/Form1.cs
+++ MIN/MIN/Form1.cs
@@ -22,6 +22,7 @@ namespace MIN
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //method to generate random numbers
             userN = Convert.ToInt16(textBox1.Text);
             numbers = new int[userN];
             Random rand = new Random();
